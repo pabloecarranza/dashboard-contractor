ea3c39fa4970a3609c41b65073e3da95b13fd699
pabloecarranza <63490339+pabloecarranza@users.noreply.github.com>
2022-12-12 21:42:39 -0300

    tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{html,jsx}'],
+	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
 		extend: {},
 	},
